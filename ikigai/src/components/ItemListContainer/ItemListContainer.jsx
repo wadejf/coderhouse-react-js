d6e0d11131e7d96d0f0da172700cc752c83f27b5
Facundo Wade <wadefacundo@gmail.com>
2023-03-26 18:49:31 -0300

    Fixed item list container retrieval

--- ikigai/src/components/ItemListContainer/ItemListContainer.jsx
+++ ikigai/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,45 +1,56 @@
 import styles from "./ItemListContainer.module.css";
 import ItemList from "../ItemList/ItemList";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { doc, getFirestore, getDoc } from 'firebase/firestore';
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {
+  collection,
+  getFirestore,
+  getDocs,
+  where,
+  limit,
+  query
+} from 'firebase/firestore';
 
 const ItemListContainer = () => {
-  const { categoryName } = useParams();
+  const {categoryName} = useParams();
   const [products, setProducts] = useState([]);
 
-  /*
-  const filterProducts = (products) => {
-    if (!categoryName)
-      return products.filter(
-        (p) => new Date(p.publishedDate) > new Date().setDate(1)
-      );
-
-    if (categoryName === "todos") return products.slice(0, 10);
+  const getQueryConstraint = () => {
+    if (categoryName === "todos")
+      return limit(10);
 
     if (categoryName === "promociones")
-      return products.filter((p) => p.discount);
+      return where('discount', '==', true);
 
-    return products;
-  };*/
+    return where('publishedDate', '>=', new Date().setDate(1));
+  };
 
   useEffect(() => {
     const db = getFirestore();
 
-    const p = doc(db, 'products', 'pa6HUvtoIn0ay5Vfan1g');
+    const c = collection(db, 'products');
 
-    const docSnap = getDoc(p);
+    const q = query(c, getQueryConstraint());
 
-    docSnap.then(s => {
-      setProducts([s.data()]);
-    }).catch(e => console.log(e));
+    const products = getDocs(q);
+
+    products.then((res) => {
+      let products = res.docs.map((product) => {
+        return {
+          ...product.data(),
+          id: product.id,
+        };
+      });
+
+      setProducts(products);
+    });
 
   }, [categoryName]);
 
   return (
-    <div className={styles.ItemListContainer}>
-      <ItemList items={products} />
-    </div>
+      <div className={styles.ItemListContainer}>
+        <ItemList items={products}/>
+      </div>
   );
 };
 
